0dde3749c726398dbe83d94f20d1822b5b9413a4
Andrii Hryhorenko <grigorenko584@gmail.com>
2023-08-29 09:35:17 +0300

    Початок роботи над проектом

---
+++ src/main/java/ua/hryhorenko/telegrambotvacancies/TelegramBotVacanciesApplication.java
@@ -0,0 +1,14 @@
+package ua.hryhorenko.telegrambotvacancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotVacanciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotVacanciesApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
