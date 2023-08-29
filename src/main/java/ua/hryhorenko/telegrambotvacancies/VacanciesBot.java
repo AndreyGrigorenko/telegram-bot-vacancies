e821535602c8db5ef3284213a4764950c706df22
Andrii Hryhorenko <grigorenko584@gmail.com>
2023-08-29 11:53:01 +0300

    Додав можливість відправляти повідомлення користувачу

--- src/main/java/ua/hryhorenko/telegrambotvacancies/VacanciesBot.java
+++ src/main/java/ua/hryhorenko/telegrambotvacancies/VacanciesBot.java
@@ -2,7 +2,9 @@ package ua.hryhorenko.telegrambotvacancies;
 
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class VacanciesBot extends TelegramLongPollingBot {
@@ -12,7 +14,16 @@ public class VacanciesBot extends TelegramLongPollingBot {
 
   @Override
   public void onUpdateReceived(Update update) {
-    System.out.println("event received");
+    String text = update.getMessage().getText();
+    System.out.println(text);
+    SendMessage sendMessage = new SendMessage();
+    sendMessage.setChatId(update.getMessage().getChatId());
+    sendMessage.setText("Your text : " + text);
+    try {
+      execute(sendMessage);
+    } catch (TelegramApiException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
